06984f2b4823b2d3e2efa12eb4c4853d6f4e5971
bakuyy <sophieyang12345@gmail.com>
2025-02-03 21:25:21 -0500

    feat: add basic landing page UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,39 @@
-import Image from "next/image";
+import { Source_Code_Pro } from "next/font/google"
+import Form from "next/form"
+import Link from "next/link"
+
+const code_pro = Source_Code_Pro({
+  display:'swap',
+  subsets:['latin'],
+  weight:['600'],
+  variable:'--font-source-code-pro'
+}) 
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-  
-    </div>
+  <div className={`${code_pro.variable} flex items-center font-code_pro justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}>       
+    <main className="ml-[5vw]">
+      <h3 className="mb-[2vh] text-3xl"> built by bakuyy </h3>
+      <div className="flex gap-4 my-[2vh]">
+        <div className="w-[1.5vw] h-[1.5vw] bg-[#0D4429] rounded-full"/>
+        <div className="w-[1.5vw] h-[1.5vw] bg-[#26A641] rounded-full"/>
+        <div className="w-[1.5vw] h-[1.5vw] bg-[#39D353] rounded-full"/>
+      </div>
+      <h1 className="text-4xl  mt-[4vh] font-extrabold tracking-tight text-white sm:text-4xl md:text-6xl"> git commit -m “<span className="text-gray-400">docs</span>: <span className="text-[#39D353]">add my stats</span>”</h1>
+
+      <div className="text-2xl mt-[3vh]">
+        <h3> {`>>`} generate a customized stats book for your github profile in seconds</h3>
+        <h3> {`>>`} show off your achievements with style and check your clan</h3>
+      </div>
+
+      <div className="flex text-2xl gap-4 mt-[4vh]">
+        <h3>Enter github user:</h3>
+        <input/>
+        <button> -{`>`}</button>
+      </div>
+
+
+    </main> 
+  </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  mode: 'jit',
+
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -12,6 +14,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily:{
+        code_pro: ['var(--font-source-code-pro)'],
+      }
     },
   },
   plugins: [],
