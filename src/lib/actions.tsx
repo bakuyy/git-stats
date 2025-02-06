f0028d3e42fc189da3b79e409bc0704f3c443a06
bakuyy <sophieyang12345@gmail.com>
2025-02-05 21:14:20 -0500

    refactor: updated structure of data fetched from github api

--- src/lib/actions.tsx
+++ src/lib/actions.tsx
@@ -9,13 +9,25 @@ export async function fetchUserData(username: string): Promise<GitHubUser | null
     const delayPromise = new Promise(resolve => setTimeout(resolve, 2000));
     
     const [data] = await Promise.all([dataPromise, delayPromise]);
+
+    const [repos, following, followers] = await Promise.all([
+        fetch(data.repos_url).then(res=>res.json()),
+        fetch(data.following_url).then(res=>res.json()),
+        fetch(data.followers_url).then(res=>res.json()),
+    ])
     
     return {
       login: data.login,
-      public_repos: data.public_repos
-    };
+      avatar_url:data.avatar_url,
+      public_repos: data.public_repos,
+      followers_count: followers.length,
+      following_count: following.length,
+      repos: repos.map((repo:any)=> ({
+        name: repo.name,
+      }))
+    }
   } catch (error) {
     console.error('Error fetching gitHub user:', error);
-    return null;
+    return null
   }
 }
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,5 +1,9 @@
 type GitHubUser = {
     login: string;
+    avatar_url:string;
+    followers_count: number;
+    following_count: number;
     public_repos: number;
+    repos:any
   }
   
